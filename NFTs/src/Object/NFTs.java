79ae4a48abac7a33a4e6618858c909bcba709798
Viet Hai <pvhmain2k3@gmail.com>
2023-11-10 17:26:51 +0700

    add method searchByDate

--- NFTs/src/Object/NFTs.java
+++ NFTs/src/Object/NFTs.java
@@ -1,5 +1,8 @@
 package Object;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NFTs {
 	private String title;
 	private String author;
@@ -30,4 +33,17 @@ public class NFTs {
 	public String getTag() {
 		return this.tag;
 	}
+
+	// Phương thức search theo date
+	public static void searchByDate(List<NFTs> nftsList, String searchDate) {
+        for (NFTs nft : nftsList) {
+            if (nft.getDate().equals(searchDate)) {
+                System.out.println("Title: " + nft.getTitle());
+                System.out.println("Author: " + nft.getAuthor());
+                System.out.println("Date: " + nft.getDate());
+                System.out.println("Tag: " + nft.getTag());
+                System.out.println("------------------------");
+            }
+        }
+    }
 }
