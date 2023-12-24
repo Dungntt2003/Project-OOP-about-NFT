784ae17f21ae433478d383d5030c19f229989a4d
unknown <nguyenthuydung55555@gmail.com>
2023-12-25 03:03:23 +0700

    Add relation

--- src/path/PathCollection.java
+++ src/path/PathCollection.java
@@ -4,7 +4,7 @@ import getdatafromjson.JsonParser;
 
 public class PathCollection {
 	JsonParser jsonParser = new JsonParser();
-	private String pathBinance = jsonParser.getJSONFromFile("D:\\java\\OOP_Nhom21\\src\\filejson\\binanceTest.json");
+	private String pathBinance = jsonParser.getJSONFromFile("D:\\java\\OOP_Nhom21\\src\\filejson\\binance.json");
 
 	public String getPathBinance() {
 		return pathBinance;
